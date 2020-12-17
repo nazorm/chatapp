2109d9fc37c0074629adb1972362885c48ed636f
Nazorm <nnadoziechinazor@gmail.com>
2020-12-17 11:40:50 +0100

    changed login button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import './App.css';
 
-class App extends React.Component{
-  constructor(){
-    super()
-    this.state = {
 
-    }
-  }
-  render(){
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: `${process.env.REACT_APP_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
+  projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_ID}`,
+  measurementId: `${process.env.REACT_APP_MEASUREMENT_ID}`
+};
+
+
+
+
+function App (){
+  
+ 
     return (
       <div className="App">
        <h1>
@@ -16,7 +26,7 @@ class App extends React.Component{
        </h1>
       </div>
     )
-  }
+  
  
 }
 
