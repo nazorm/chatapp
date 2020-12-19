b6b25b2ebddee859c7bd19bf0e73cd6a55636d3d
Nazorm <nnadoziechinazor@gmail.com>
2020-12-19 20:16:49 +0100

    added components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
-import React from 'react'
-import './App.css';
-import React, { useEffect, useRef, useState } from 'react';
 
+
+import React, { useEffect, useRef, useState } from 'react';
+import './App.css';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
-import SignIn from 'Components/SignIn'
-import SignOut from 'Components/SignOut'
+import SignIn from './components/SignIn'
+import SignOut from './components/SignOut'
+import ChatRoom from './components/ChatRoom'
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-//const firebaseConfig = 
-firebase.initializeApp({
-  apiKey: `${process.env.REACT_APP_API_KEY}`,
-  authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
-  projectId: `${process.env.REACT_APP_PROJECT_ID}`,
-  storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
-  messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
-  appId: `${process.env.REACT_APP_ID}`,
-  measurementId: `${process.env.REACT_APP_MEASUREMENT_ID}`
-});
+
 
 
 const auth = firebase.auth();
@@ -37,6 +28,9 @@ function App (){
         <h1>prochat</h1>
         <SignOut/>
       </header>
+      <section>
+        {user? <ChatRoom/> : <SignIn/>}
+      </section>
       </div>
     )
   
