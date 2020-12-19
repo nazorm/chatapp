766be849669101da08634825ce554ad4953d4f5e
Nazorm <nnadoziechinazor@gmail.com>
2020-12-19 19:45:10 +0100

    created chat room for messages

---
+++ src/Components/ChatRoom.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ChatRoom=()=>{
+    return(
+        <div>
+            <h1>
+                Chat Room
+            </h1>
+        </div>
+    )
+}
+
+export default ChatRoom
