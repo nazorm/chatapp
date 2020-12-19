df40573502ed82b2e466f1df0b4cbf5b4e8e35ed
Nazorm <nnadoziechinazor@gmail.com>
2020-12-19 20:17:43 +0100

    worked on new user sign in

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -3,6 +3,25 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+//const firebaseConfig = 
+firebase.initializeApp({
+    apiKey: `${process.env.REACT_APP_API_KEY}`,
+    authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
+    projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+    storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
+    messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
+    appId: `${process.env.REACT_APP_ID}`,
+    measurementId: `${process.env.REACT_APP_MEASUREMENT_ID}`
+  });
+
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 const SignIn = ()=>{
 
 
