5fbfc4efd26ed79bf16b052921bf8670853bbfff
Nazorm <nnadoziechinazor@gmail.com>
2020-12-19 20:18:51 +0100

    worked on sign out button

--- src/Components/SignOut.js
+++ src/Components/SignOut.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+
+
+
+
+let auth = firebase.auth();
 
 const SignOut = ()=>{
 
    return auth.currentUser && (
+       <div>
       <button onClick={() => auth.signOut()}>Sign Out</button>
+      </div>
     )
 }
 
